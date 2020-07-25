64f47a20c740dd638a2eb11cf76869169d794674
Dania King <dania@fortunebuilders.com>
2020-07-24 19:59:31 -0700

    added modals to images
    Signed-off-by: Dania King <dania@fortunebuilders.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
-import {MDBContainer} from "mdbreact";
+import { MDBContainer} from 'mdbreact';
 import Header from "./components/Header"
 import Card from "./components/Card"
 import "./index.css";
 
+
+
 class App extends Component
 {
+  
   render ()
   {
     return (
       <body>
         <MDBContainer className="w-responsive text-center mx-auto p-3 mt-2">
           <Header />
-          <Card/>
+          <Card />
 
 
         </MDBContainer>
